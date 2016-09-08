e4f3cc1b0794e55cdd1eac195654d62e0968eb69
janbe30 <janbe30@gmail.com>
2016-09-07 22:54:55 -0500

    updates made

--- scripts.js
+++ scripts.js
@@ -1,17 +1,50 @@
+
+/* Initial function that grabs data from todo.json file */
 $(function(){
   $.getJSON('/home/janbe30/Documents/learnJS/node-app/todo.json', function(data){
     $.each(data, function(i, f){
-      $("<p  contenteditable=\"false\">" + f.id + "</p>").appendTo("#todos #ids");
-      $("<p contenteditable=\"false\">" + f.title + "</p>").appendTo("#todos #titles");
-      $("<p contenteditable=\"false\">" + f.description + "</p>").appendTo("#todos #descriptions");
-      $("<p contenteditable=\"false\">" + f.completed + "</p>").appendTo("#todos #completeds");
+      $("<p>" + f.id + "</p>").appendTo("#todos #ids");
+      $("<p>" + f.title + "</p>").appendTo("#todos #titles").on('click', handleClick);
+      $("<p>" + f.description + "</p>").appendTo("#todos #descriptions").on('click', handleClick);
+      $("<p>" + f.completed + "</p>").appendTo("#todos #completeds").on('click', handleClick);
     });
   });
 });
 
+/* Function that turns each item <p> to <input> when user clicks on it */
+var handleClick = function(){
+      //console.log($(this).text());
+      var html = "<input value='" + $(this).text() + "'>";
+      $(this).replaceWith(html);
+      listenEnter();
+};
 
-function changeToInput() {
-      $('.intro ').click(function () {
-          $('.intro').attr("contenteditable", "true");
-      });
-}
+/* Function converts <input> to <p> after user updates item and presses Enter */
+var listenEnter = function(){
+  $('#todos input').keyup(function (event){
+    var currentItem = this;
+    //console.log(currentItem);
+  if (event.keyCode == '13') {
+    event.preventDefault();
+      inputtoP(currentItem);
+  }
+  return false;
+  });
+};
+
+var inputtoP = function (item) {
+  //console.log($(item).val());
+  var newHTML = "<p>" + $(item).val() + "</p>";
+  $(item).replaceWith(newHTML);
+
+};
+
+
+$.ajax({
+  method: "PUT",
+  url: "/home/janbe30/Documents/learnJS/node-app/todo.json",
+  data: { name: "John", location: "Boston" }
+})
+  .done(function( msg ) {
+    alert( "Data Saved: " + msg );
+  });
